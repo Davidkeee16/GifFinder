0aa93b6da27e7a8ebb9fa9b33d3ae6afc05dffcb
David <davidkeee16@gmail.com>
2025-10-11 11:39:03 +0300

    Initial Commit

---
+++ GifFinderUITests/GifFinderUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  GifFinderUITestsLaunchTests.swift
+//  GifFinderUITests
+//
+//  Created by David Puksanskis on 11/10/2025.
+//
+
+import XCTest
+
+final class GifFinderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
